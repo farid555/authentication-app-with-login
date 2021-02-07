35336ae7c95d88dcf1b2b76a2339ba862c473d95
farid555 <mohammad.uzzaman2@centria.fi>
2021-02-07 02:16:54 +0200

    Connect Mongoose...

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-
+const mongoose = require('mongoose')
 
 const app = express()
+//Db config
 
+const db = require('./config/keys').MongoURI;
+
+// connect
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Mongoose Connected...'))
+    .catch(err => console.log(err))
 
 //EJS
 app.use(expressLayouts)
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://login:login@cluster0.xqfbe.mongodb.net/loninfo?retryWrites=true&w=majority'
+}
